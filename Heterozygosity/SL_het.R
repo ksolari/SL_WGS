efcb8b71d442adb4f69e8fc2c02d504a37facbb0
ksolari <ksolari@stanford.edu>
2025-08-27 16:10:20 -0700

    Update SL_het.R

--- Heterozygosity/SL_het.R
+++ Heterozygosity/SL_het.R
@@ -8,55 +8,10 @@ cbPalette = c("captive" = "black","India" = "#E51932","Pakistan" = "#FF99BF","Af
               "Kyrgyzstan" = "#19B2FF", "Russia" = "#A5EDFF", "Mongolia (unknown)" = "#32FF00", "Mongolia (NW)" = "#B2FF8C",
               "Mongolia (SW)" = "#FF7F00", "Mongolia (South)" = "#FFBF7F")
 
-####Final het - no 10x
-het_practice <- read.csv('SL_HET_gencove_vs_ownCalls.csv',head=T)
-
-het_practice <- data.frame(het_practice)
-
-het_practice <- het_practice %>%
-  mutate( Location=factor(Location,levels=c("captive", "India", "Pakistan", "Afghanistan", "Tajikistan", "Kyrgyzstan", "Russia", "Mongolia (unknown)", "Mongolia (NW)", "Mongolia (SW)", "Mongolia (South)")) )
-
-p1<-ggplot(het_practice, aes(x=Location, y=Obs_Het_gencove_finalDenominator, color=Location)) + 
-  geom_boxplot() +
-  geom_jitter(height = 0, width = 0.1, alpha = 0.5) +
-  scale_color_manual(name = "Location", values = cbPalette) +
-  labs(y="Observed Heterozygosity gencove") +
-  theme_classic()+
-  ylim(0,0.00025)+ 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 13), 
-        axis.text.y = element_text(size = 10, hjust = 1, vjust = 1), 
-        axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 13),
-        strip.text = element_text(size = 13),
-        panel.grid.major.y = element_line(color = "grey90"),
-        legend.position="none",
-        plot.margin = margin(.5, .5, .5, .5, "cm")) 
-p1
-
-p2<-ggplot(het_practice, aes(x=Location, y=Obs_Het_ours_finalDenom, color=Location)) + 
-  geom_boxplot() +
-  geom_jitter(height = 0, width = 0.1, alpha = 0.5) +
-  scale_color_manual(name = "Location", values = cbPalette) +
-  labs(y="Observed Heterozygosity ours") +
-  theme_classic()+
-  ylim(0,0.00025)+ 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 13), 
-        axis.text.y = element_text(size = 10, hjust = 1, vjust = 1), 
-        axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 13),
-        strip.text = element_text(size = 13),
-        panel.grid.major.y = element_line(color = "grey90"),
-        legend.position="none",
-        plot.margin = margin(.5, .5, .5, .5, "cm")) 
-p2
-
-
-
-figuregarlic_v1 <- (p1 | p2 )
-figuregarlic_v1
-
 
 #####linear comparison of the two values sets
+het <- read.csv('SL_HET_gencove_vs_ownCalls.csv',head=T)
+het <- data.frame(het)
 library(ggpubr)
 library(ggplot2)
 
@@ -78,20 +33,56 @@ p2<-ggplot(het_practice, aes(x=Obs_Het_ours_finalDenom, y=Obs_Het_gencove_finalD
 p2
 
 ###plot for main figure 2 of ms
-p2<-ggplot(het_practice, aes(x=Location, y=Obs_Het_gencove_finalDenominator, color=Location)) + 
-  geom_boxplot() +
-  geom_jitter(height = 0, width = 0.1, alpha = 0.5) +
-  scale_color_manual(name = "Location", values = cbPalette) +
-  labs(y="Observed Heterozygosity") +
-  theme_classic()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 13), 
-        axis.text.y = element_text(size = 10, hjust = 1, vjust = 1), 
-        axis.title.x = element_blank(),
+cbPalette = c("Jaguar" = "grey60", "Lion" = "grey60", "Amur Tiger" = "grey60", "Bengal Tiger" = "grey60","Sumatran Tiger" = "grey60", "Malayan Tiger" = "grey60", "Puma" = "grey30", 
+              "Cheetah" = "grey30", "Snow Leopard" = "gold3",  "Asian Leopard" = "grey60", "African Leopard" = "grey50")
+
+
+het <- read.csv("Panthera_Observed_het_vcftools_JagEdited_SL28Recalc.csv", header = T)
+p <- ggplot(het, aes(x=Species, y=Observed_Heterozygosity_SL_lowDP_removed, color = Species, fill = Species)) + 
+  geom_boxplot(alpha=0.5) +
+  theme(legend.position="none") +
+  xlab("Species") + ylab("Observed Heterozygosity") 
+
+p + scale_y_continuous() + 
+  scale_x_discrete(limits=c("African Leopard", "Asian Leopard", "Jaguar", "Lion", "Amur Tiger", "Bengal Tiger","Sumatran Tiger", "Malayan Tiger", "Snow Leopard", "Cheetah", "Puma" ))+
+  theme_classic()+ 
+  ylim(0,0.0023)+ 
+  scale_fill_manual(name = "Species", values = cbPalette) +
+  scale_color_manual(name = "Species", values = cbPalette) +
+  geom_jitter(height = 0, width = 0.1, alpha = 0.5, cex=1.5) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1,size = 11), 
+        axis.text.y = element_text(size = 11), 
+        axis.title.x = element_text(size = 13),
         axis.title.y = element_text(size = 13),
         strip.text = element_text(size = 13),
+        legend.position = "none",
         panel.grid.major.y = element_line(color = "grey90"),
-        legend.position="none",
-        plot.margin = margin(.5, .5, .5, .5, "cm")) 
-p2
+        plot.margin = margin(.5, 1, .5, .5, "cm"))
+
+
+het_df <- data.frame(het)
+
+SLsubet <- subset.data.frame(het_df, Species == "Snow Leopard")
+JagSubset <- subset.data.frame(het_df, Species == "Jaguar")
+AfLeopSubset <- subset.data.frame(het_df, Species == "African Leopard")
+AsLeopSubset <- subset.data.frame(het_df, Species == "Asian Leopard")
+Lionsubset <- subset.data.frame(het_df, Species == "Lion")
+AmurSubset <- subset.data.frame(het_df, Species == "Amur Tiger")
+BenSubset <- subset.data.frame(het_df, Species == "Bengal Tiger")
+SumSubset <- subset.data.frame(het_df, Species == "Sumatran Tiger")
+MalSubset <- subset.data.frame(het_df, Species == "Malayan Tiger")
+pumaSubset <- subset.data.frame(het_df, Species == "Puma")
+CheSubset <- subset.data.frame(het_df, Species == "Cheetah")
+
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, JagSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, AfLeopSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, AsLeopSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, Lionsubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, AmurSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, BenSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, SumSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, MalSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, pumaSubset$Observed_Heterozygosity_SL_lowDP_removed)
+wilcox.test(SLsubet$Observed_Heterozygosity_SL_lowDP_removed, CheSubset$Observed_Heterozygosity_SL_lowDP_removed)
 
 
